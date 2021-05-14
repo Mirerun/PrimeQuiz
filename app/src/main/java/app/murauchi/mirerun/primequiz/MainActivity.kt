7b9a05cc44c53836087140fa2efa24f5455dce43
mirei <mirei.saxophone1010@gmail.com>
2021-05-15 01:51:02 +0900

    丸訂正

--- app/src/main/java/app/murauchi/mirerun/primequiz/MainActivity.kt
+++ app/src/main/java/app/murauchi/mirerun/primequiz/MainActivity.kt
@@ -35,42 +35,42 @@ class MainActivity : AppCompatActivity() {
 
         textView.text = questions[answerCount].toString() + ""
         textView.setTextColor(Color.BLACK)
+    }
 
-        fun maru(v: View) {
-            var answer = true //boolean型
-            val number = questions[answerCount]
+    fun maru(v: View) {
+        var answer = true //boolean型
+        val number = questions[answerCount]
 
-            for (i in 2 until number) { //2以上number未満のi
-                if (number % i == 0) { //iで割り切れたら素数じゃない->false
-                    answer = false
-                    break //一度素数でないと判定したらその後調べる必要ない
-                }
+        for (i in 2 until number) { //2以上number未満のi
+            if (number % i == 0) { //iで割り切れたら素数じゃない->false
+                answer = false
+                break //一度素数でないと判定したらその後調べる必要ない
             }
-            if (answer) { //if answer==true
-                Toast.makeText(this,"正解", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this,"不正解", Toast.LENGTH_SHORT).show()
-            }
+        }
+        if (answer) { //if answer==true
+            Toast.makeText(this,"正解", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this,"不正解", Toast.LENGTH_SHORT).show()
+        }
 
-            if (answer) {
-                point++ //+1の意味
-                Log.d("maru","正解" + point.toString())
-            } else {
-                Log.d("maru","不正解")
-            }
+        if (answer) {
+            point++ //+1の意味
+            Log.d("maru","正解" + point.toString())
+        } else {
+            Log.d("maru","不正解")
+        }
 
-            answerCount++ //正解数を入れるanswerCountに+1の意味
+        answerCount++ //正解数を入れるanswerCountに+1の意味
 
-            if (answerCount == QUESTION_COUNT) { //問題が終わったかどうか
-                textView.text = point.toString() + "点" //終わってたら点数表示
-                textView.setTextColor(Color.RED)
+        if (answerCount == QUESTION_COUNT) { //問題が終わったかどうか
+            textView.text = point.toString() + "点" //終わってたら点数表示
+            textView.setTextColor(Color.RED)
 
-                point = 0 //次回のために得点と解答数を初期化
-                answerCount = 0
-            } else {
-                textView.text = questions[answerCount].toString() + ""
-                textView.setTextColor(Color.BLACK)
-            }
+            point = 0 //次回のために得点と解答数を初期化
+            answerCount = 0
+        } else {
+            textView.text = questions[answerCount].toString() + ""
+            textView.setTextColor(Color.BLACK)
         }
     }
 }
